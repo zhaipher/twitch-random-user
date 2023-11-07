c9c58d3a4db1b855c759facb32e415a74d2b0334
zhaipher <78917478+zhaipher@users.noreply.github.com>
2023-11-07 21:10:26 +0700

    Update randomviewer.py

--- randomviewer.py
+++ randomviewer.py
@@ -1,11 +1,9 @@
-import random
-def random_user():
-    users = []
-    for user in chat.users:
-        users.append(user.name)
-    return random.choice(users)
-def response(message):
-    user = random_user()
-    return f": {user}"
-if message.content == "!roll":
-    response(message)
+import twitchio
+client = twitchio.Client()
+@client.event("message")
+async def on_message(message):
+    if message.content == "!roll":
+        users = await client.get_users(limit=1)
+        user = users[0]
+        await client.send_message(message.channel, f": {user.name}")
+client.run()
