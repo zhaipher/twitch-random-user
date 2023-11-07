8c1625ddca80c412ba656afa6ec4eb0e01cad20b
zhaipher <78917478+zhaipher@users.noreply.github.com>
2023-11-07 21:08:27 +0700

    Update randomviewer.py

--- randomviewer.py
+++ randomviewer.py
@@ -1,14 +1,11 @@
 import random
-
 def random_user():
     users = []
     for user in chat.users:
         users.append(user.name)
     return random.choice(users)
-
 def response(message):
     user = random_user()
-    return f"ผู้โชคดีคือ: {user}"
-
+    return f": {user}"
 if message.content == "!roll":
     response(message)
