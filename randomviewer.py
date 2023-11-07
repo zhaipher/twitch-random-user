e59d9ba5327a21402d18fa85acc86fe0f2d9d7f2
zhaipher <78917478+zhaipher@users.noreply.github.com>
2023-11-07 20:45:53 +0700

    Add files via upload

---
+++ randomviewer.py
@@ -0,0 +1,12 @@
+import twitchio
+
+client = twitchio.Client()
+
+@client.event("message")
+async def on_message(message):
+    if message.content == "!roll":
+        users = await client.get_users(limit=1, status="online")
+        user = users[0]
+        await client.send_message(message.channel, f"สุ่มผู้โชคดี: {user.name}")
+
+client.run()
