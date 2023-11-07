734cdaaab12b7e0d304b0c37484dcc14853904ad
zhaipher <78917478+zhaipher@users.noreply.github.com>
2023-11-07 20:52:23 +0700

    Update randomviewer.py

--- randomviewer.py
+++ randomviewer.py
@@ -5,7 +5,7 @@ client = twitchio.Client()
 @client.event("message")
 async def on_message(message):
     if message.content == "!roll":
-        users = await client.get_users(limit=1, status="online")
+        users = await client.get_users(limit=1)
         user = users[0]
         await client.send_message(message.channel, f"สุ่มผู้โชคดี: {user.name}")
 
